7b35a892552eed7f8a999222cf85b30e610dd8ce
MahmudovMuhammadrizo24 <mahmudovmuhammadrizo877@gmail.com>
2023-05-15 21:36:25 +0500

    change typescript code

--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
-const incriment = document.querySelector<HTMLDivElement>(".incriment");
-const dicriments = document.querySelector<HTMLDivElement>(".dicriment");
-const schet = document.querySelector<HTMLDivElement>(".schet");
-const xstop = document.querySelector<HTMLButtonElement>(".x");
-const checkbox = document.querySelector<HTMLInputElement>(".checkbox");
-const button = document.querySelector<HTMLLabelElement>(".button");
-const timer = document.querySelector<HTMLInputElement>(".time");
-const step = document.querySelector<HTMLInputElement>(".step");
 
+/*
+const incriment = document.querySelector('.incriment');
+const dicriments = document.querySelector('.dicriment');
+const schet = document.querySelector('.schet');
+const xstop = document.querySelector('.x');
+const checkbox = document.querySelector('.checkbox');
+const button = document.querySelector('.button');
+const timer = document.querySelector('.time');
+const step = document.querySelector('.step');
 
 checkbox?.addEventListener('change', () => {
-    if (checkbox.checked) {
+    if (checkbox?.checked) {
         button?.classList.add('checked');
         console.log('Animation triggered!');
     } else {
@@ -18,15 +19,14 @@ checkbox?.addEventListener('change', () => {
     }
 });
 
-function getInctiment(): void {
-
-    incriment?.addEventListener("click", () => {
+function getInctiment() {
+    incriment?.addEventListener('click', () => {
         schet.textContent = String(Number(step.value) + Number(schet.textContent));
     });
 }
 
-function getDicriment(): void {
-    dicriments?.addEventListener("click", () => {
+function getDicriment() {
+    dicriments?.addEventListener('click', () => {
         if (schet.textContent === '0') {
             schet.textContent = '0';
         } else {
@@ -35,30 +35,22 @@ function getDicriment(): void {
     });
 }
 
-function getXstop(): void {
-    xstop?.addEventListener("click", () => {
+function getXstop() {
+    xstop?.addEventListener('click', () => {
         checkbox.checked = false;
         console.log('Hello');
     });
 }
 
-function getTime(): void {
-    const timeValue = timer.value;
-    if (timeValue !== '0') {
-
-    }
-}
-
-function getStep(): void {
+function getStep() {
     const stepValue = step.value;
     if (stepValue !== '0') {
         schet.textContent += stepValue;
     }
 }
 
-function init(): void {
+function init() {
     getStep();
-    getTime();
     getXstop();
     setTimeout(() => {
         getDicriment();
@@ -66,4 +58,101 @@ function init(): void {
     }, Number(timer.value) * 100);
 }
 
+init();
+*/
+
+const increment = document.querySelector('.increment') as HTMLButtonElement;
+const decrement = document.querySelector('.decrement') as HTMLButtonElement;
+const counter = document.querySelector('.counter') as HTMLSpanElement;
+const xstop = document.querySelector('.x') as HTMLButtonElement;
+const checkbox = document.querySelector('.checkbox') as HTMLInputElement;
+const button = document.querySelector('.button') as HTMLDivElement;
+const timer = document.querySelector('.time') as HTMLInputElement;
+const step = document.querySelector('.step') as HTMLInputElement;
+
+checkbox?.addEventListener('change', () => {
+    if (checkbox?.checked) {
+        button?.classList.add('checked');
+        console.log('Animation triggered!');
+    } else {
+        button?.classList.remove('checked');
+        console.log('Hello');
+    }
+});
+
+function getIncrement(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        increment?.addEventListener('click', () => {
+            counter.textContent = String(Number(step.value) + Number(counter.textContent));
+            resolve();
+        });
+    });
+}
+
+function getDecrement(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        decrement?.addEventListener('click', () => {
+            if (counter.textContent === '0') {
+                counter.textContent = '0';
+            } else {
+                counter.textContent = String(Number(counter.textContent) - Number(step.value));
+            }
+            resolve();
+        });
+    });
+}
+
+function getXstop(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        xstop?.addEventListener('click', () => {
+            checkbox.checked = false;
+            console.log('Hello');
+            resolve();
+        });
+    });
+}
+
+function getStep(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        const stepValue = step.value;
+        if (stepValue !== '0') {
+            counter.textContent = String(Number(stepValue) + Number(counter.textContent));
+        }
+        resolve();
+    });
+}
+/*
+async function init(): Promise<void> {
+    await getStep();
+    await getXstop();
+    if (checkbox?.checked === true) {
+        setTimeout(async () => {
+
+            await getDecrement();
+            await getIncrement();
+        }, Number(timer.value) * 100);
+    }
+}
+*/
+
+
+function getTimer(): number {
+    const timerValue = Number(timer.value);
+    return !isNaN(timerValue) ? timerValue : 0;
+}
+
+function sleep(ms: number): Promise<void> {
+    return new Promise<void>((resolve) => setTimeout(resolve, ms));
+}
+
+async function init(): Promise<void> {
+    await getStep();
+    await getXstop();
+
+    const timerValue = getTimer() * 1000;
+    if (timerValue > 0) {
+        await sleep(timerValue);
+        await getIncrement();
+    }
+}
 init();
